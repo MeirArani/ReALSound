cd1d3c08d8473fa76c6ad0e66a940a3f081f246b
Zachary Arani <cloud12817@gmail.com>
2024-12-23 09:25:10 +0900

    more reworks

--- cv/widgets/__init__.py
+++ cv/widgets/__init__.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtCore import Qt, Slot
+from PySide6.QtCore import Qt, Slot, QMargins
 from PySide6.QtWidgets import (
     QApplication,
     QDialog,
@@ -20,18 +20,21 @@ class CVSlider(QHBoxLayout):
         super(CVSlider, self).__init__(parent)
 
         self.label = QLabel(f"{settings["name"] or "_"}: {settings["value"]}")
-
+        self.label.setScaledContents(True)
+        # self.label.setContentsMargins(QMargins(10, 10, 10, 10))
         self.slider = QSlider(Qt.Orientation.Horizontal)
         self.slider.setMinimum(settings["min"] if "min" in settings else 100)
         self.slider.setMaximum(settings["max"])
-        self.slider.singleStep = settings["step"] if "step" in settings else 1
+        self.slider.setSingleStep(settings["step"] if "step" in settings else 1)
+        self.slider.setTickInterval(settings["tick"] if "tick" in settings else 1)
+        self.slider.setTickPosition(QSlider.TickPosition.TicksAbove)
         self.slider.value = (
             settings["value"] if "value" in settings else self.slider.value
         )
         self.slider.valueChanged.connect(self.on_move)
-
-        self.addWidget(self.label)
-        self.addWidget(self.slider)
+        self.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.addSpacing(20)  # TODO:find stretch issue
+        self.addWidget(self.slider, alignment=Qt.AlignmentFlag.AlignRight)
 
     @Slot(int)
     def on_move(self, val):
